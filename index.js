d228c082dc8eaaccfb6bce997c6d99774d78d1fb
MD Taukir Alam <taukir007alam@gmail.com>
2023-08-09 22:07:28 +0600

    added data in the local port

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
-const port = process.ENV.PORT || 5000;
+const port = process.env.PORT || 5000;
+app.use(cors());
+
+const data = require("./data.json");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(data);
 });
 
 app.listen(port, () => {
