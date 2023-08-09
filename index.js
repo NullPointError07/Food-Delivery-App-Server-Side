d67f6c7acb2cc5f7fe7f7dc64c27f013fdc228f0
MD Taukir Alam <taukir007alam@gmail.com>
2023-08-10 03:08:57 +0600

    updated data with interior images

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.get("/", (req, res) => {
   res.send(data);
 });
 
+app.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedData = data.find((n) => n.id === id);
+
+  if (selectedData) {
+    res.send(selectedData);
+  } else {
+    res.status(404).send("data not found");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
